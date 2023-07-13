31e5fe766066e1ee23f5fa8e2398af7c132b2a6b
Juan Piedra <juan.piedra@rocketmail.com>
2023-07-13 10:58:03 -0700

    started server, added login

---
+++ controllers/homeroutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage", {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+    try {
+      res.render("login", {});
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
