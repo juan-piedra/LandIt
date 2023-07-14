c2513377bf863845f4c4d8a8dee7a894fb65a9bf
Accurast <siavash65n@gmail.com>
2023-07-13 20:22:16 -0700

    discard

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -4,7 +4,8 @@ const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("homepage", {});
+  //   const loginData = await Gallery.findAll(
+    res.render("homepage");
   } catch (err) {
     res.status(500).json(err);
   }
